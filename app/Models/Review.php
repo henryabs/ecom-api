c3a4f3c10a4bae086e878eb5e173a44b97883327
Henry Abayan <h.abayan@yahoo.com>
2021-02-27 06:11:59 +0800

    Relationship created

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
 class Review extends Model
 {
     use HasFactory;
+
+    public function product(){
+    	return $this->belongsTo(Product::class, 'product_id', 'id');
+    }
 }
